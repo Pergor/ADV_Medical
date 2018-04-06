96c014edba46df8d01be022b111f7e60d8c92430
Belbo <adrian_verscharen@yahoo.de>
2018-04-06 16:26:40 +0200

    MERGED: adv_aceCPR and adv_aceSplint into adv_aceMedical.

---
+++ @adv_aceMedical/mod.cpp
@@ -0,0 +1,13 @@
+name = "ADV - ACE Medical";
+picture = "logo.paa";
+actionName = "Website";
+action = "http://spezialeinheit-luchs.de";
+logo = "logo.paa";
+logoOver = "logo.paa";
+tooltip = "ADV - ACE Medical";
+tooltipOwned = "ADV - ACE Medical";
+overview = "This is a combination of ADV - ACE CPR and ADV - ACE Splint.";
+author = "SeL-Belbo";
+overviewPicture = "logo.paa";
+overviewText = "ADV - ACE MEdical";
+overviewFootnote = "";
